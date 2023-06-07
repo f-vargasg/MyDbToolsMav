ebb22833c2f528972cc87be2554506c28dc8b7a4
Francisco Vargas G. <52474105+f-vargasg@users.noreply.github.com>
2023-06-07 14:07:23 -0600

    Primera version

--- src/main/java/com/fvgprinc/tools/common/app/dbconnection2/DbConnFactory.java
+++ src/main/java/com/fvgprinc/tools/common/app/dbconnection2/DbConnFactory.java
@@ -14,10 +14,10 @@ import java.sql.SQLException;
  * @author garfi
  */
 public class DbConnFactory {
-    private static  DbConn dbConn;
-    
-    public static DbConn getDbConn (DbTypes dbTypes) throws SQLException, CommonDALExceptions
-    {
+
+    private static DbConn dbConn;
+
+    public static DbConn getDbConn(DbTypes dbTypes) throws SQLException, CommonDALExceptions {
         if (dbConn != null) {
             return dbConn;
         }
@@ -30,5 +30,29 @@ public class DbConnFactory {
         }
         return dbConn;
     }
-            
+
+    /*
+    public static DbConn getDbConn() throws SQLException, CommonDALExceptions {
+        DbTypes dbTypes;
+
+        /*
+        Ir a la configuración y leer el tipo de Base de datos
+        y la configuración de cada DB
+        
+        */
+    /*
+        
+        if (dbConn != null) {
+            return dbConn;
+        }
+        switch (dbTypes) {
+            case MariaDb:
+                dbConn = new MariaDbConn();
+                break;
+            default:
+                dbConn = null;
+        }
+        return dbConn;
+    }
+*/
 }
